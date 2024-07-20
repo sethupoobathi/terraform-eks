53948da995ba24cd754a1188d69d432f95dc0601
sethu123 <143830594+sethupoobathi@users.noreply.github.com>
2024-07-20 13:09:43 +0530

    Update vpc link.tf

--- vpc link.tf
+++ vpc link.tf
@@ -1,5 +1,1 @@
-resource “aws_api_gateway_vpc_link” “main” {
- name = “foobar_gateway_vpclink”
- description = “Foobar Gateway VPC Link. Managed by Terraform.”
- target_arns = [var.load_balancer_arn]
-}
+
